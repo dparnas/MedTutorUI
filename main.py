658f20fee6c26449c136df5970e1390db528b9c8
dparnas <75569862+dparnas@users.noreply.github.com>
2026-02-12 15:13:58 +0200

    Try empty load_dotenv

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 
 # load_dotenv(r'C:\Users\dovid\PycharmProjects\MedTutorUI\variable.env')
-
+load_dotenv()
 app = FastAPI()
 
 # ---- LLM client ----
